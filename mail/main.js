e9c33e83124e8863a90e97b411642e0916ae652d
Gargenziano03 <gargenziano03@gmail.com>
2024-10-03 18:13:23 +0200

    add fix

--- mail/main.js
+++ mail/main.js
@@ -33,9 +33,11 @@ console.log(mail);
 // controlla se nella list è presenta la mail inserita
 if(list.includes(mail)){
 // se è presente stampa accesso consentiti
+    alert('accesso consentito');
     console.log('accesso consentito');
 // altrimenti stampa accesso negato
 }else{
+    alert('accesso negato');
     console.log('accesso negato');
 }
 
