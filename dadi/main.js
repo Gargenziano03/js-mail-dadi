e7c7ea23c49e12a3bbb415a7e9dc31b14c63ddfc
Gargenziano03 <gargenziano03@gmail.com>
2024-10-03 17:02:36 +0200

    add dati phase 1 and 2

---
+++ dadi/main.js
@@ -0,0 +1,21 @@
+/*Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+
+/*pre-fase: STRUMENTI
+-- array
+-- variabili 
+-- .random 
+-- if, else if, else
+--.floor
+--.random
+*/
+
+//fase1: PREPARAZIONE //fase2: RACCOLTA DATI
+const giocatore = Math.floor(Math,random() * 7);
+const computer = Math.floor(Math.random() * 7);
+
+//fase3: ELABORAZIONE DEI DATI 
+
+
+
