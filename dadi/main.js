1b1c17c58efb760f0deb0191e925368f3999345c
Gargenziano03 <gargenziano03@gmail.com>
2024-10-03 17:14:16 +0200

    add dadi phase 3

--- dadi/main.js
+++ dadi/main.js
@@ -12,10 +12,30 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
 */
 
 //fase1: PREPARAZIONE //fase2: RACCOLTA DATI
-const giocatore = Math.floor(Math,random() * 7);
+// creazione della variabile
+// aggiungo match.random per i numeri casuali 
+// aggiungo match.floor per far in modo che i numeri siano interi e non con la virgola
+const giocatore = Math.floor(Math.random() * 7);
 const computer = Math.floor(Math.random() * 7);
 
 //fase3: ELABORAZIONE DEI DATI 
+// se il giocatore ha il numero più alto
+if(giocatore > computer){
+// stampa messaggio
+    console.log('il vincitore è l utente')
+// stampa il numero del giocatore 
+    console.log(giocatore)
+// se il computer ha il numero più alto
+}else if(giocatore < computer){
+// stampa il messaggio
+    console.log('il vincitore è il computer');
+// stampa il numero del computer
+    console.log(computer);
+// altrimenti in caso i due numeri siano uguali
+}else(
+// stampa il messaggio
+    console.log('pareggio')
+)
 
 
 
